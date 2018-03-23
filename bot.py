55d6e9ae551dc6a1c54d985ba07a054642145cc4
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-03-23 23:52:26 +0900

    add: add send_captcha function

--- bot.py
+++ bot.py
@@ -1,4 +1,12 @@
+import requests
 from slackbot.bot import Bot
+
+def send_captcha(access_token, channel_id, image_name)
+    files = {'file': open(image_name, 'rb')}
+    param = {'token': access_token, 'channels': channel_id}
+    res = requests.post(url="https://slack.com/api/files.upload", params=param, files=files)
+    if res.json()['ok']:
+        return res.json()['file']
 
 def main():
     bot = Bot()
