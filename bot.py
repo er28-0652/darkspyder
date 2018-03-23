691c63615d262dc47c647d5abb8f157bc02bec80
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-03-24 00:44:31 +0900

    update

--- bot.py
+++ bot.py
@@ -1,13 +1,6 @@
 import requests
 from slackbot.bot import Bot
 
-def send_captcha(access_token, channel_id, image_name)
-    files = {'file': open(image_name, 'rb')}
-    param = {'token': access_token, 'channels': channel_id}
-    res = requests.post(url="https://slack.com/api/files.upload", params=param, files=files)
-    if res.json()['ok']:
-        return res.json()['file']
-
 def main():
     bot = Bot()
     bot.run()
