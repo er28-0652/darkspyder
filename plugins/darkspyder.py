902ee3ae28b0898b4593afd5b34faa0e99bd42f2
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-03-26 22:44:07 +0900

    update: add parse()

--- plugins/darkspyder.py
+++ plugins/darkspyder.py
@@ -1,6 +1,8 @@
 import base64
 import re
+
 import requests
+from bs4 import BeautifulSoup
 
 class TorClient:
     def __init__(self, host='127.0.0.1', port=9050):
@@ -38,19 +40,19 @@ class DarkSpyder:
 class WallStMarket(DarkSpyder):
     def __init__(self, url, username, password):
         super().__init__(url)
+        self.current_page = ""
         self.username = username
         self.password = password
         self.cap_data_ptn = re.compile(r'<img class="captcha_image" id="[^"]+" src="data:image/jpeg;base64,([^"]+)"')
         self.cap_token_ptn = re.compile(r'<input type="hidden" id="form__token" name="form\[_token\]" value="([^"]+)" />')
         self.is_pass_security = False
-        self.is_login = False
 
     def start(self):
-        return self.client.get(self.url+'/index').text
+        self.current_page = self.client.get(self.url+'/index').text
 
-    def get_captcha(self, html):
-        m_data = self.cap_data_ptn.search(html)
-        m_token = self.cap_token_ptn.search(html)
+    def get_captcha(self):
+        m_data = self.cap_data_ptn.search(self.current_page)
+        m_token = self.cap_token_ptn.search(self.current_page)
 
         if m_data and m_token:
             cap_b64= m_data.groups()[0]
@@ -58,16 +60,19 @@ class WallStMarket(DarkSpyder):
             cap_token = m_token.groups()[0]
             return cap_token, cap_image_data
     
-    def pass_security_check(self, cap_token, cap_word):
+    def _pass_security_check(self, cap_token, cap_word):
         payload = {'form[_token]': cap_token, 'form[captcha]': cap_word}
         html = self.client.post(self.url+'/index', data=payload).text
-        if self.is_login_page(html):
+        self.current_page = html
+
+        if self._is_login_page:
             self.is_pass_security = True
-            return html
+            return True
+        return False
         
     def login(self, cap_token, cap_word):
         if self.is_pass_security is False:
-            return self.pass_security_check(cap_token, cap_word)
+            return self._pass_security_check(cap_token, cap_word)
 
         payload = {
             'form[_token]': cap_token,
@@ -78,11 +83,29 @@ class WallStMarket(DarkSpyder):
             'form[language]': 'en',
             'form[pictureQuality]': '1'
         }
-        res = self.client.post(self.url+'/login', data=payload)
-        self.is_login = True
-        return res.text
+        html = self.client.post(self.url+'/login', data=payload).text
+        self.current_page = html
+        return self.is_login
+
+    def parse(self):
+        soup = BeautifulSoup(self.current_page, 'lxml')
+        categories = soup.find_all('button', {'name':'menuCatT'})
+
+        results = {}
+        for cat in categories:
+            texts = cat.get_text().split()
+            number = texts[-1]
+            category = ''.join(texts[:-1])
+            results[category] = int(number)
+        return results
+
+    @property
+    def is_login(self):
+        soup = BeautifulSoup(self.current_page, 'lxml')
+        nav = soup.select('span.nav-link')
+        return len(nav) != 0
     
-    @staticmethod
-    def is_login_page(html):
+    @property
+    def _is_login_page(self):
         login_title = '<h3 class="wms_captcha_field wsm_pbc_headmod">Log In</h3>'
-        return login_title in html
+        return login_title in self.current_page
