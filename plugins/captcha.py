c0021a09cf736417db5150a1c404f9adb62222ae
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-03-26 09:45:16 +0900

    update

--- plugins/captcha.py
+++ plugins/captcha.py
@@ -1,27 +1,49 @@
+import io
+import pickle
+from datetime import datetime
 from slackbot.bot import respond_to
+from .darkspyder import WallStMarket
+from .setting import target_markets
 
-def send_captcha(access_token, channel_id, image_name):
-    files = {'file': open(image_name, 'rb')}
-    param = {'token': access_token, 'channels': channel_id}
-    res = requests.post(url="https://slack.com/api/files.upload", params=param, files=files)
-    if res.json()['ok']:
-        return res.json()['file']
+clients = {}
 
+def send_captcha(message, image_data, image_id):
+    channels = message._body['channel']
+    channels = ','.join(channels) if isinstance(channels, (tuple, list)) else channels
+    data = {
+        'initial_comment': 'なにこれ？ #{0}'.format(image_id),
+        'channels': channels
+    }
+    message._client.webapi.files.post(
+        'files.upload',
+        data=data,
+        files={'file': io.BytesIO(image_data)}
+    )
 
-@respond_to(r'^ダークスクレイピング\s+\S.*')
+@respond_to(r'^働け！')
 def start_scraping(message):
-    message.send('scraping...')
-    msg = message.body['text']
-    url = text.split(None, 1)[1]
-    wsm = WallStMarket(url)
-    image_path = wsm.save_captcha_image()
-    message.send_webapi()
+    message.send('ダーーーク！スクレイッピングッッッ！！！')
 
+    for target in target_markets:
+        market = target['class']
+        url = target['url']
+        message.send('アクセス中 {0}'.format(url))
+        client = market(url, target['username'], target['password'])
+        html = client.start()
+        cap_token, image_data = client.get_captcha(html)
 
-@respond_to(r'.+')
-def read_captcha_answer(message):
-    answer = message.body['text']
-    print(answer)
-    print(message.channel)
-    message.reply('thank you')
+        send_captcha(message, image_data, cap_token)
 
+        global clients
+        clients[cap_token] = client
+
+@respond_to(r'^#(.*) (.*)')
+def break_captcha(message, cap_token, answer):
+    client = clients[cap_token]
+    html = client.login(cap_token, answer)
+    if client.is_login is False:
+        cap_token, image_data = client.get_captcha(html)
+        send_captcha(message, image_data, cap_token)
+        clients[cap_token] = client
+    else:
+        message.send('いっぱいちゅき❤️')
