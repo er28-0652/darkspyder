57a0cb7cc5a93867e6b544acca600fc3d2b47ea6
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-04-07 20:28:57 +0900

    fix: modify some bug

--- plugins/darkspyder.py
+++ plugins/darkspyder.py
@@ -114,13 +114,12 @@ class WallStMarket(DarkSpyder):
         return self.client.post(self.url+'/index', data=payload).text
 
     @staticmethod
-    def _read_table(table):
-        df = pd.read_html(page_html)[1]
+    def _read_table(page):
+        df = pd.read_html(page)[1]
         df['Level'] = df['Vendor'].apply(lambda vendor: vendor.split()[-1])
         df['Vendor'] = df['Vendor'].apply(lambda vendor: vendor.split()[0])
         df['Price'] = df['Price'].apply(lambda price: price.split()[-1].split('/')[0])
-        df = df.drop('#', axis=1)
-        return df
+        return df.drop('#', axis=1)
 
     def parse(self):
         soup = BeautifulSoup(self.current_page, 'lxml')
@@ -130,18 +129,16 @@ class WallStMarket(DarkSpyder):
             category_id = cat.get('value')
             texts = cat.get_text().split()
             category = ' '.join(texts[:-1])
-            result = pd.DataFrame()
             # currently Software & Malware is only supported
             if category == 'Software & Malware':
                 number = texts[-1]
+                result = pd.DataFrame()
                 # read each page
                 for page in range(1, int(int(number)/90)+2):
-                    page_html = self._read_page(client, category_id, str(page))
-                    #page_soup = BeautifulSoup(page_html, 'lxml')
-                    #table = page_soup.find(class_='table table-bordered table-striped table-xxsm table-logs')
+                    page_html = self._read_page(category_id, str(page))
                     pruduct_table = self._read_table(page_html)
                     result = result.append(pruduct_table, ignore_index=True)
-            return result
+                return result
 
     @property
     def is_login(self):
